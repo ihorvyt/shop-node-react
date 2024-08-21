7325f39b260772b399a2accc907a596e37a7f806
ihorvyt <ihorvyt@gmail.com>
2024-08-21 17:37:27 +0300

    first commit

---
+++ server/models/models.js
@@ -0,0 +1,17 @@
+const sequlize = require('../db');
+const {DataTypes} = require('sequelize');
+
+const User = sequlize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'},
+})
+
+
+const Basket = sequlize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'},
+})
